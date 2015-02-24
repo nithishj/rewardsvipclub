3ece26761b37c431b909b5a7904a39e652da8f51
ganesh <ganeswararao.sundarapu@gmail.com>
2015-02-24 23:13:46 +0530

    star rewards list users fixes

--- includes/ngadmin/js/controllers/custom/rewardsCtrl.js
+++ includes/ngadmin/js/controllers/custom/rewardsCtrl.js
@@ -1,4 +1,4 @@
-app.controller('rewardsCtrl',['$scope','$location','RewardsFactory', function($scope, $location,RewardsFactory)
+app.controller('rewardsCtrl',['$scope','$location','$modal','RewardsFactory', function($scope, $location,$modal,RewardsFactory)
 {
 
      $scope.rewards_lists = [];
@@ -16,7 +16,27 @@ app.controller('rewardsCtrl',['$scope','$location','RewardsFactory', function($s
     $scope.get_rewards_list();
    
 
-
+    //$scope.rewards_users_lists
+    $scope.openUsersList = function(usersList,size){
+        $scope.rewards_users_lists = usersList;
+         var modalInstance = $modal.open({
+            templateUrl: 'includes/ngadmin/tpl/rewards_user_list_modal.html',
+            controller: 'rewardUsersCtrl',
+            size: size,
+            scope:$scope
+        });
+        
+    };
 
 
 }]);
+
+
+app.controller('rewardUsersCtrl', ['$scope', '$location', '$modalInstance','RewardsFactory',
+  function($scope, $location, $modalInstance, RewardsFactory) {
+      
+      $scope.user_cancel = function(){
+        $modalInstance.close();
+      };
+      
+  }]);
