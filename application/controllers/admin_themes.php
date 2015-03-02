74c23f278cea28ee2d4c5c9fa65deb0bdbf4597e
nithishj <nithish.reddy.jnh@gmail.com>
2015-03-02 23:23:31 +0530

    admin themes api

---
+++ application/controllers/admin_themes.php
@@ -0,0 +1,54 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class admin_themes extends CI_Controller
+{
+
+   function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('url'); 
+		
+	}
+
+	function addtheme()
+	{
+
+		$json = json_decode(trim(file_get_contents('php://input')),true);
+		if(!empty($json['Image']) && !empty($json['UserId']))
+		{
+		
+		$d = array('Image'  => $json['Image'],
+		  		   'UserId' => $json['UserId']);
+		$this->db->insert("theme_lookup",$d);
+        echo json_encode(array("message"=>"success","code"=>200));
+		}
+		else
+		echo json_encode(array("message"=>"Required Fields","code"=>400));
+	}
+
+	function deletetheme()
+	{
+		$json = json_decode(trim(file_get_contents('php://input')),true);
+		if(!empty($json['ThemeId']))
+		{
+          $this->db->delete('theme_lookup',array('theme_lookup_id'=>$json['ThemeId']));
+          echo json_encode(array("message"=>"success","code"=>200));
+		}
+		else
+	    echo json_encode(array("message"=>"Required Fields","code"=>400));
+
+	}
+
+	function listthemes()
+	{
+        $base=base_url();
+        $thumb=$base."resize.php?height=50&width=50&path=";
+        $q=$this->db->query("select concat('$base',Image) as theme,concat('$thumb',Image) as thumb from theme_lookup order by theme_lookup_id desc");
+        if($q->num_rows()>0)
+        echo json_encode($q->result());
+        else 
+        echo json_encode(array("message"=>"Required Fields","code"=>400));
+
+	}
+}
